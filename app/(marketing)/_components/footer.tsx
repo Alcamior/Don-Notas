d186982593fa9023a478ec97d13b9ebd02ae2af7
Camila Alor <alorcamila2@gmail.com>
2025-04-08 10:57:06 -0600

    Se quitarón apartados de términos y condiciones.

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -5,16 +5,6 @@ export const Footer = () => {
 return (
     <div className="flex items-center w-full p-6 bg-background z-50">
         <Logo />
-        <div className="md:ml-auto w-full justify-between
-        md:justify-end flex items-center gap-x-2
-        text-muted-foreground">
-            <Button variant="ghost" size="sm">
-                Políticas de privacidad
-            </Button>
-            <Button variant="ghost" size="sm">
-                Términos y condiciones
-            </Button>
-        </div>
     </div>
 )
 
